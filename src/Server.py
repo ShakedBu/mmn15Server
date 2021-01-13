e686e6498387cad1b21f0e5657da45420719e1e5
ShakedBu <buch.shaked@gmail.com>
2021-01-13 16:27:04 +0200

    alot more

--- src/Server.py
+++ src/Server.py
@@ -1,38 +1,68 @@
 import socket
-import ssl
+import selectors
+import struct
+from src.User import User
 
 
 class Server:
     def __init__(self):
         with open('port.info', 'r') as server_file:
             line = server_file.readline()
-            self.host = line
+            self.port = int(line)
         self.host = '127.0.0.1'
+        self.selector = selectors.DefaultSelector()
+        self.users = []
+
+    def accept(self, sock, mask):
+        conn, address = sock.accept()  # Should be ready
+        conn.setblocking(False)
+        self.selector.register(conn, selectors.EVENT_READ, self.read)
+
+    def read(self, conn, mask):
+        data = conn.recv(1024)  # Should be ready
+        print(data)
+
+        if data:
+            format_s = "<16s B B I {}s".format(len(data) - 22)
+            client_id, version, code, size, payload = struct.unpack(format_s, data)
+            print(client_id, version, code, payload)
+
+            # Register new User
+            if code == 100:
+                new_user = User(repr(payload))
+                self.users.append(new_user)
+                response = struct.pack('B H I 16s', 1, 1000, 16, new_user.id.bytes)
+                conn.send(response)
+            else:
+                # Check registered user
+
+                # Return users list
+                if code == 101:
+
+                    pass
+                # Public key
+                elif code == 102:
+                    pass
+                # Send message
+                elif code == 103:
+                    pass
+                # Return waiting messages
+                elif code == 104:
+                    pass
+
+        else:
+            self.selector.unregister(conn)
+            conn.close()
 
     def start(self):
-        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-        context.load_cert_chain(certfile="mycertfile", keyfile="mykeyfile")
-
         bind_socket = socket.socket()
         bind_socket.bind((self.host, self.port))
-        bind_socket.listen(5)
+        bind_socket.listen()
+        bind_socket.setblocking(False)
+        self.selector.register(bind_socket, selectors.EVENT_READ, self.accept)
 
         while True:
-            new_socket, from_addr = bind_socket.accept()
-            conn_stream = context.wrap_socket(new_socket, server_side=True)
-            try:
-                self.deal_with_client(conn_stream)
-            finally:
-                conn_stream.shutdown(socket.SHUT_RDWR)
-                conn_stream.close()
-
-    def deal_with_client(self, conn_stream):
-        data = conn_stream.recv(1024)
-        # empty data means the client is finished with us
-        while data:
-            #if not do_something(conn_stream, data):
-                # we'll assume do_something returns False
-                # when we're finished with client
-            #    break
-            data = conn_stream.recv(1024)
-        # finished with client
+            events = self.selector.select()
+            for key, mask in events:
+                callback = key.data
+                callback(key.fileobj, mask)
--- src/User.py
+++ src/User.py
@@ -1,5 +1,17 @@
+import uuid
+import struct
+
+
 class User:
-    def __init__(self, u_id, public_key, last_seen):
+    def __init__(self, name, u_id=None, public_key=None, last_seen=None):
+        if u_id is None:
+            u_id = uuid.uuid1()
+        self.name = name
         self.id = u_id
         self.public_key = public_key
         self.last_seen = last_seen
+
+    def get_user_bytes(self):
+        format_s = '16s {}s'.format(len(self.name))
+        user = struct.pack(format_s, self.id.bytes, self.name)
+        return user
