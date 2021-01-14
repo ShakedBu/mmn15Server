475b130e05b70c3504f455a098dabf0729e2bc16
ShakedBu <buch.shaked@gmail.com>
2021-01-14 14:49:47 +0200

    add users list action

--- src/Server.py
+++ src/Server.py
@@ -2,6 +2,7 @@ import socket
 import selectors
 import struct
 import uuid
+import sys
 from src.User import User
 
 
@@ -12,7 +13,7 @@ class Server:
             self.port = int(line)
         self.host = '127.0.0.1'
         self.selector = selectors.DefaultSelector()
-        self.users = []
+        self.users = {}
 
     def accept(self, sock, mask):
         conn, address = sock.accept()  # Should be ready
@@ -20,40 +21,57 @@ class Server:
         self.selector.register(conn, selectors.EVENT_READ, self.read)
 
     def read(self, conn, mask):
-        data = conn.recv(1024)  # Should be ready
-        print(data)
+        try:
+            data = conn.recv(1024)  # Should be ready
 
-        if data:
-            format_s = "<16s B B I {}s".format(len(data) - 22)
-            client_id, version, code, size, payload = struct.unpack(format_s, data)
-            print(client_id, version, code, payload)
+            if data:
+                format_s = "<16s B B I {}s".format(len(data) - 22)
+                client_id, version, code, size, payload = struct.unpack(format_s, data)
 
-            # Register new User
-            if code == 100:
-                new_user = User(repr(payload))
-                self.users.append(new_user)
-                response = struct.pack('B H I 16s', 1, 1000, 16, new_user.id.bytes)
-                conn.send(response)
+                # Register new User
+                if code == 100:
+                    new_user = User(conn, str(payload, 'utf-8'))
+                    self.users[new_user.id] = new_user
+                    response = struct.pack('B H I 16s', 1, 1000, 16, new_user.id.bytes)
+                    conn.send(response)
+
+                else:
+                    # Check registered user
+                    curr_uid = uuid.UUID(bytes=client_id)
+
+                    # User not registered - return an error
+                    if curr_uid not in self.users:
+                        response = struct.pack('B H I', 1, 9000, 0)
+                        print(response)
+                        conn.send(response)
+
+                    # Return users list
+                    if code == 101:
+                        payload = b""
+                        for user in self.users:
+                            # TODO: don't return current user- just for making testing easy
+                            # if user != curr_uid:
+                            payload = b"".join([payload, self.users[user].get_user_bytes()])
+                        format_s = 'B H I {}s'.format(len(payload))
+                        response = struct.pack(format_s, 1, 1001, len(payload), payload)
+                        print(response)
+                        conn.send(response)
+
+                    # Public key
+                    elif code == 102:
+                        pass
+                    # Send message
+                    elif code == 103:
+                        pass
+                    # Return waiting messages
+                    elif code == 104:
+                        pass
+
             else:
-                # Check registered user
-
-                # Return users list
-                if code == 101:
-                    uid = uuid.UUID(bytes=client_id)
-                    # TODO: Check if there is such user and get him a list
-
-                    pass
-                # Public key
-                elif code == 102:
-                    pass
-                # Send message
-                elif code == 103:
-                    pass
-                # Return waiting messages
-                elif code == 104:
-                    pass
-
-        else:
+                self.selector.unregister(conn)
+                conn.close()
+        except:
+            print("Unexpected error:", sys.exc_info())
             self.selector.unregister(conn)
             conn.close()
 
@@ -68,4 +86,5 @@ class Server:
             events = self.selector.select()
             for key, mask in events:
                 callback = key.data
+
                 callback(key.fileobj, mask)
--- src/User.py
+++ src/User.py
@@ -3,7 +3,8 @@ import struct
 
 
 class User:
-    def __init__(self, name, u_id=None, public_key=None, last_seen=None):
+    def __init__(self, connection, name, u_id=None, public_key=None, last_seen=None):
+        self.connection = connection
         if u_id is None:
             u_id = uuid.uuid1()
         self.name = name
@@ -12,6 +13,6 @@ class User:
         self.last_seen = last_seen
 
     def get_user_bytes(self):
-        format_s = '16s {}s'.format(len(self.name))
-        user = struct.pack(format_s, self.id.bytes, self.name)
+        # TODO: this will probably wont work
+        user = struct.pack('16s 255s', self.id.bytes, bytes(self.name.ljust(255), 'utf-8'))
         return user
